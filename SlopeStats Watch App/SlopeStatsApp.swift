6f35170beb8630a7bd29ff691dd1420dda00db7f
Nick Hageman <nickhageman8@gmail.com>
2024-12-22 14:31:26 -0600

    Initial commit, first watchOS project

---
+++ SlopeStats Watch App/SlopeStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlopeStatsApp.swift
+//  SlopeStats Watch App
+//
+//  Created by Alex Hageman on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SlopeStats_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlopeStats/SlopeStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlopeStatsApp.swift
+//  SlopeStats
+//
+//  Created by Alex Hageman on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SlopeStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
